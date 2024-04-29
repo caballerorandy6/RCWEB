ab87efda264f49fde7a5e57b2ed8573d28023040
Randy Caballero <112903057+caballerorandy6@users.noreply.github.com>
2024-04-29 01:16:56 -0500

    Updated ExperienceComponent.tsx, added mb-24

--- src/app/ui/components/ExperienceComponent.tsx
+++ src/app/ui/components/ExperienceComponent.tsx
@@ -5,7 +5,7 @@ import ArrowDownIcon from "@/app/ui/home/hero/icons/ArrowDownIcon";
 
 const ExperienceComponent = () => {
   return (
-    <Link href="#experience" className="flex justify-center items-center">
+    <Link href="#experience" className="flex justify-center items-center mt-24">
       <motion.h2
         className="p-2 rounded-lg flex justify-center items-center gap-2 text-3xl text-donkeybrown font-concertOne hover:text-gold  transition-colors"
         variants={linkVariants}
